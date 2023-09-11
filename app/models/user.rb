79f1a90fb2cdfc542ff808637825b80c7268275b
abel-tefera <abeltefera16@gmail.com>
2023-09-11 12:47:20 +0300

    Configured action mailer to send welcome email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,13 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
+
+  after_commit :send_welcome_email, on: :create
+
+  private
+
+  def send_welcome_email
+    UserMailer.with(user: self).welcome_email.deliver_later
+  end
 end
