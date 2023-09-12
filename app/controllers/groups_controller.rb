b18ea14e029a975e7f7af426947c03f655c13246
abel-tefera <abeltefera16@gmail.com>
2023-09-12 14:32:21 +0300

    Groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,28 +1,22 @@
 class GroupsController < ApplicationController
-  before_action :set_group, only: %i[show destroy]
-  # before_action :authenticate_user!
+  before_action :set_group, only: %i[destroy]
 
   def index
-    # @groups = Group.where(author_id: current_user.id).includes(:expenses).order('created_at DESC')
-    @groups = Group.where(author_id: current_user.id).order('created_at DESC')
+    @groups = Group.includes(:expenses).where(user: current_user).order('created_at ASC')
+  end
+
+  def show
+    @group = Group.includes(:expenses).find(params[:id])
   end
 
   def new
     @group = Group.new
   end
 
-  def show
-    # if current_user.id.to_i == Group.find(params[:id]).author_id.to_i
-    # @group = Group.find(params[:id])
-    # else
-    # redirect_to '/'
-    # end
-  end
-
   def create
     @group = Group.new(group_params)
     @group.name = @group.name.capitalize
-    @group.author_id = current_user.id
+    @group.user = current_user
 
     if @group.save
       redirect_to groups_url, notice: 'Category was successfully created'
