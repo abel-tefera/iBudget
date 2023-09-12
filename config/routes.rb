8e0959498007ec9ed99619a7ff12bcd6ce66f4ef
abel-tefera <abeltefera16@gmail.com>
2023-09-12 14:25:30 +0300

    Routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'expenses/index'
-  get 'expenses/new'
-  get 'expenses/create'
+  # get 'expenses/index'
+  # get 'expenses/new'
+  # get 'expenses/create'
   # get 'groups/index'
   # get 'groups/new'
   # get 'groups/show'
@@ -12,7 +12,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :groups, only: [:index, :new, :create, :show, :destroy]
+  resources :groups, only: [:index, :new, :create, :show, :destroy] do
+    resources :expenses, only: [:new, :create]
+  end
 
   root to: 'home#index'
 end
