5441a635cd6eda792c7bb9ca62f4010ff801f8d3
abel-tefera <abeltefera16@gmail.com>
2023-09-12 09:09:37 +0300

    controller methods set up

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,13 +1,54 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: %i[show destroy]
+  # before_action :authenticate_user!
+
   def index
+    # @groups = Group.where(author_id: current_user.id).includes(:expenses).order('created_at DESC')
+    @groups = Group.where(author_id: current_user.id).order('created_at DESC')
   end
 
   def new
+    @group = Group.new
   end
 
   def show
+    # if current_user.id.to_i == Group.find(params[:id]).author_id.to_i
+    # @group = Group.find(params[:id])
+    # else
+    # redirect_to '/'
+    # end
   end
 
   def create
+    @group = Group.new(group_params)
+    @group.name = @group.name.capitalize
+    @group.author_id = current_user.id
+
+    if @group.save
+      redirect_to groups_url, notice: 'Category was successfully created'
+    else
+      render :new, status: :unprocessable_entity, alert: 'An error has occurred while creating'
+    end
+  end
+
+  def destroy
+    @group.destroy
+
+    respond_to do |format|
+      format.html { redirect_to groups_url, notice: 'Group was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_group
+    @group = Group.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def group_params
+    params.require(:group).permit(:name, :icon)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
-  get 'groups/index'
-  get 'groups/new'
-  get 'groups/show'
-  get 'groups/create'
+  # get 'groups/index'
+  # get 'groups/new'
+  # get 'groups/show'
+  # get 'groups/create'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :groups, only: [:index, :new, :create, :show, :destroy]
+
   root to: 'home#index'
 end
