88e23148c3389745e843bf4414d53317207a32bc
abel-tefera <abeltefera16@gmail.com>
2023-09-12 09:09:08 +0300

    Groups controller generated

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,13 @@
+class GroupsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'groups/index'
+  get 'groups/new'
+  get 'groups/show'
+  get 'groups/create'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
