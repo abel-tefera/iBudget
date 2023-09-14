49b3c4b5223aa54bce699c33e4a485f660a48c0a
abel-tefera <abeltefera16@gmail.com>
2023-09-14 10:49:24 +0300

    ActionMailer production fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,20 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # config.relative_url_root = "/iBudget"
+
+  config.action_mailer.default_url_options = { host: '145.14.158.81', port: 80 }
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'example.com',
+      user_name:            Rails.application.credentials.email.user,
+      password:             Rails.application.credentials.email.password,
+      authentication:       'plain',
+      enable_starttls_auto: true,
+      open_timeout:         5,
+      read_timeout:         5 
+  }
 end
